ae2d1f3d3920ccbdb9607aa10175376dcdde9e7a
ThongNguyen <nguyenthuongthongcm@gmail.com>
2018-09-02 22:54:13 +0700

    Initial Commit

---
+++ CinemaHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CinemaHub
+//
+//  Created by Tran Tuyen on 9/2/18.
+//  Copyright © 2018 ninjaKID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
