728267f623b7801c45f4edcbe65e445955e044e3
ThongNguyen <nguyenthuongthongcm@gmail.com>
2018-09-03 14:10:08 +0700

    Install lib and config network layer

---
+++ CinemaHub/Network/MockApi.swift
@@ -0,0 +1,13 @@
+//
+//  MockApi.swift
+//  CinemaHub
+//
+//  Created by Tran Tuyen on 9/3/18.
+//  Copyright © 2018 ninjaKID. All rights reserved.
+//
+
+import UIKit
+
+class MockApi: NSObject {
+
+}
